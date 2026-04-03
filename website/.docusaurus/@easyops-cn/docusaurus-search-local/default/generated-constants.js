c406d7bbc1788931a168493de1fac45c9a949624
fernandoartalf <fernandoartalf@users.noreply.github.com>
2026-04-03 17:51:16 +0000

    deploy: 8812e727eb6c89eaf074f62f347c449f561d8994

--- website/.docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,6 +0,0 @@
-import lunr from "C:\\Users\\FernandoArtigasAlfon\\Documents\\AL\\al-copilot-skills-site\\website\\node_modules\\lunr\\lunr.js";
-export const removeDefaultStopWordFilter = [];
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=2c0b7d45";
-export const searchResultLimits = 10;
-export const fuzzyMatchingDistance = 1;
--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\FernandoArtigasAlfon\\Documents\\AL\\al-copilot-skills-site\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\FernandoArtigasAlfon\\Documents\\AL\\al-copilot-skills-site\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\FernandoArtigasAlfon\\Documents\\AL\\al-copilot-skills-site\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\FernandoArtigasAlfon\\Documents\\AL\\al-copilot-skills-site\\website\\src\\css\\custom.css"),
-];
